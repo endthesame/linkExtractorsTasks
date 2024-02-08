70530eaa982c2596e65e7d91d517634ecb5cbe02
eterentiev <endthesame@gmail.com>
2024-02-08 09:30:19 +0300

    disable sandbox

--- rsc/journals/rsc_journals.js
+++ rsc/journals/rsc_journals.js
@@ -10,7 +10,7 @@ async function extractLinks(page) {
 }
 
 async function crawlPages(startUrl) {
-    const browser = await puppeteer.launch({ headless: 'new' });
+    const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.goto(startUrl, { waitUntil: 'networkidle0', timeout: 50000 });
 
