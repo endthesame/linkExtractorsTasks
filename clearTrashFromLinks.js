6fc6bc202d13f5a1f2630ba476493a827cb2de37
eterentev <eterentev@tesyan.ru>
2024-03-21 16:20:07 +0300

    rsc books; duke books

--- clearTrashFromLinks.js
+++ clearTrashFromLinks.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 const file = fs.readFileSync('rsc/books/found_links_rsc_books.txt', 'utf8').split('\n');
 
 // Фильтрация ссылок
-const filteredLinks = file.filter(link => !link.includes("adclick.g.doubleclick"));
+const filteredLinks = file.map(link => link.replace("?searchresult=1", ""));
 
 // Запись отфильтрованных ссылок в новый файл
 fs.writeFileSync('rsc/books/found_links_rsc_books.txt', filteredLinks.join('\n'), 'utf8');
 
-console.log('Файл filteredLinks.txt успешно создан');
+console.log('Файл успешно изменен');
