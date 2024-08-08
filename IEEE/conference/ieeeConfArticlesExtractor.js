88e9f67b557292b2aee8b0c83d1e454dad451e48
eterentiev <endthesame@gmail.com>
2024-08-08 23:01:20 +0300

    fix extractor

--- IEEE/conference/ieeeConfArticlesExtractor.js
+++ IEEE/conference/ieeeConfArticlesExtractor.js
@@ -39,10 +39,14 @@ async function crawlPages(startUrl, page) {
     logger.info(`Processing main URL: ${startUrl}`);
 
     while (true) {
-        await page.waitForSelector('#result-list-start');
+        let currUrl = page.url();
+        try{
+            await page.waitForSelector('#result-list-start');
+        } catch {
+            await page.goto(currUrl, { waitUntil: 'networkidle2', timeout: 50000 });
+        }
         var rawLinks = await isNextButtonExist(page);
         const contentLinks = Array.from(new Set([...rawLinks]));
-        let currUrl = page.url();
         fs.appendFileSync('found_links_IEEE_conf_articles.txt', contentLinks.join('\n') + '\n');
 
         logger.info(`Links from Page ${currUrl}; links found: ${contentLinks.length}`);
@@ -69,7 +73,7 @@ async function crawlPages(startUrl, page) {
 async function main() {
     const sourceLinksPath = 'links_to_extract.txt';
     const sourceLinks = fs.readFileSync(sourceLinksPath, 'utf-8').split('\n').filter(Boolean);
-    const browser = await puppeteer.launch({ headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 1080 });
 
